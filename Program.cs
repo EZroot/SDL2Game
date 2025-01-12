ab9ac4de7a4d55cdabec6384fd94a3935edde7cc
EZRoot <utohaha@gmail.com>
2025-01-12 09:29:44 -0330

    Working Game example for SDl2Engine

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SDL2Engine.Core;
+using SDL2Game;
+
+public static class Program
+{
+    public static void Main()
+    {
+        var app = new GameApp();
+        app.Run(new MyGame());
+    }
+}
